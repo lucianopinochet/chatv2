717b93d0c818d2a6d55eb975214e232706e7ac7c
Luciano <lucianobenjaminpm@gmail.com>
2023-08-08 22:08:18 -0400

    started with clap

--- client/src/main.rs
+++ client/src/main.rs
@@ -3,22 +3,36 @@ use std::process::exit;
 use std::thread;
 use std::time::Duration;
 use tokio::io::{self, AsyncWriteExt, BufReader, AsyncReadExt};
-
+use clap::Parser;
 use tokio::net::TcpStream;
+
+///Program to connect for chat
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+  ///Name of the user (Obligatory)
+  #[arg(short, long, default_value = "Luciano")]
+  name: String,
+}
 
 #[tokio::main]
 async fn main(){
-  let args:Vec<String> = env::args().collect();
-  if args.len() != 2{
-    exit(1);
-  }
+  
+    let args:Vec<String> = env::args().collect();
+  
+  let args = Args::parse();
+  
+  /// if args.len() != 2{
+  
+  /// ///exit(1);
+  ///}
 
   let mut client = TcpStream::connect("localhost:3000").await.unwrap();
   println!("Messages: "); 
     tokio::spawn(async move {
       let (reader, mut writer) = client.split();
 
-      writer.write_all(&args[1].as_bytes()).await.unwrap();
+      writer.write_all(&args.name.as_bytes()).await.unwrap();
 
       let mut data = [0;64];
 
