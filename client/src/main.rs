ef4e9fa5bdde500d4fc483ed68d2199f69d06011
lucianopinochet <lucianobenjaminpm@gmail.com>
2023-08-27 20:21:43 -0400

    made code bit prittier

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,11 +1,9 @@
-use std::env;
 use std::process::exit;
 use std::thread;
-use std::time::Duration;
 use tokio::io::{self, AsyncWriteExt, BufReader, AsyncReadExt};
-use clap::Parser;
 use tokio::net::TcpStream;
-
+use std::time::Duration;
+use clap::Parser;
 ///Program to connect for chat
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -14,41 +12,25 @@ struct Args {
   #[arg(short, long, default_value = "Luciano")]
   name: String,
 }
+    
 
 #[tokio::main]
 async fn main(){
-  
-    let args:Vec<String> = env::args().collect();
-  
   let args = Args::parse();
-  
-  /// if args.len() != 2{
-  
-  /// ///exit(1);
-  ///}
-
-  let mut client = TcpStream::connect("localhost:3000").await.unwrap();
+  let mut client = TcpStream::connect("localhost:3001").await.unwrap();
   println!("Messages: "); 
     tokio::spawn(async move {
       let (reader, mut writer) = client.split();
-
       writer.write_all(&args.name.as_bytes()).await.unwrap();
-
       let mut data = [0;64];
-
       let mut buffer = [0;64];
-
       let mut reader = BufReader::new(reader);
-
       let mut stdin = io::stdin();
-      
       loop{
         tokio::select! {
           result = reader.read(&mut buffer) => {
-              
             println!("{}", String::from_utf8_lossy(&buffer));
             buffer = [0;64];
-            
             if let Err(_) = result{
               println!("Connection Broked");
               exit(0);
@@ -56,7 +38,6 @@ async fn main(){
             thread::sleep(Duration::from_millis(100));
           }
           _ = stdin.read(&mut data) => {
-
             writer.write_all(&data).await.expect("Error writing to stream");
             data = [0;64];
           }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,84 +1,54 @@
-use std::process::exit;
-
 use tokio::{
 	net::TcpListener, 
 	sync::broadcast,
-	io::{self, BufReader, AsyncBufReadExt, AsyncWriteExt, AsyncReadExt},
+	io::{ BufReader, AsyncBufReadExt, AsyncWriteExt, AsyncReadExt},
 	};
 #[tokio::main]
 async fn main(){
-  let listener = TcpListener::bind("localhost:3000").await.unwrap();
-
+  let listener = TcpListener::bind("localhost:3001").await.unwrap();
 	let (tx, _rx) = broadcast::channel(10);
-	
-	let mut cli = [0;64];
-
 	loop{
 		let (mut stream, socket_addr) = listener.accept().await.unwrap();
-	
 		let tx = tx.clone();
-
 		let mut rx = tx.subscribe();
-
-		
 		tokio::spawn(async move {
 			let mut name = [0;16];
-	
 			stream.read(&mut name).await.unwrap();
-	
 			let name = String::from_utf8_lossy(&name).to_string();
-
 			println!("{name} Connected.");
-			
-
 			let (reader, mut writer) = stream.split();
-			// writer.write_all("hola".as_bytes()).await.unwrap();
 			let mut reader = BufReader::new(reader);
-			
 			let mut line = String::new();
-
-
-      let mut stdin = io::stdin();
-
+			println!("almost loop");
 			loop {
+			println!("in loop");
 					tokio::select! {
-						result = reader.read_line(&mut line) => {
-							
+						result = reader.read_line(&mut line)=> {
+							println!("reading from stream");
 							if let Err(_) = result{
 								println!("Connection lost with {name}");
 								break;
+							}						
+							if line.len() == 0{
+								break;
 							}
 							let mut line = format!("{}: {}", &name, &line);
-							
+							println!("{line}"); 
 							tx.send((line.clone(), socket_addr)).unwrap();
-
 							line.clear();
+							println!("end reading from stream");
 						}
 						result = rx.recv() => {
-
+              println!("sending to stream");
 							let (msg, other_addr) = result.unwrap();
-							
 							let msg = msg.trim_end_matches('\n');
-
 							if other_addr == socket_addr {
 							} 
 							if other_addr != socket_addr {
 								let msg = msg.as_bytes();
 								writer.write_all(msg).await.unwrap();
 							}
-						}
-						_ = stdin.read(&mut cli) => {
-							
-							let msg = cli.into_iter().take_while(|&x| x != 0).collect::<Vec<u8>>();
-							
-							cli = [0;64];
-
-							let msg = String::from_utf8_lossy(&msg).to_string();
-
-							if msg.len() == 2{
-								exit(0);
-							}
-
+							println!("end sending to stream");
 						}
 					}
 			}
